feff6edcd5921ccaf07251a6033c315597aba8d2
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:51:39 +0530

    Create 2215-Finding3DigitEvenNumbers

---
+++ 2215-Finding3DigitEvenNumbers/2215-Finding3DigitEvenNumbers.cpp
@@ -0,0 +1,31 @@
+// Last updated: 5/28/2025, 9:51:39 PM
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> ans;
+        vector<int> count(10);
+
+        for (const int digit : digits)
+            ++count[digit];
+
+        for (int a = 1; a <= 9; ++a) {
+            if (count[a] == 0)
+                continue;
+            count[a]--;
+            for (int b = 0; b <= 9; ++b) {
+                if (count[b] == 0)
+                    continue;
+                count[b]--;
+                for (int c = 0; c <= 8; c += 2) {
+                    if (count[c] == 0)
+                        continue;
+
+                    ans.push_back(a * 100 + b * 10 + c);
+                }
+                count[b]++;
+            }
+            count[a]++;
+        }
+        return ans;
+    }
+};
