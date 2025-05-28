acb505b4cc5117c1734c3455591aaf3bc684d571
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:56:02 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 5/28/2025, 9:56:01 PM
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int ans = 0;
+        int end = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (farthest >= nums.size() - 1) {
+                ++ans;
+                break;
+            }
+            if (i == end) {
+                ++ans;
+                end = farthest;
+            }
+        }
+
+        return ans;
+    }
+};
