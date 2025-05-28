6847f481925b91bfed41737a8a2e586e0e5a9bfc
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:49:04 +0530

    Create 3291-FindIfArrayCanBeSorted

---
+++ 3291-FindIfArrayCanBeSorted/3291-FindIfArrayCanBeSorted.cpp
@@ -0,0 +1,27 @@
+// Last updated: 5/28/2025, 9:49:03 PM
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        int prevSetBits = 0;
+        int prevMax = INT_MIN;
+        int currMax = INT_MIN;
+        int currMin = INT_MAX;
+
+        for (const int num : nums) {
+            const int setBits = __builtin_popcount(num);
+            if (setBits != prevSetBits) {
+                if (prevMax > currMin)
+                    return false;
+                prevSetBits = setBits;
+                prevMax = currMax;
+                currMax = num;
+                currMin = num;
+            } else {
+                currMax = max(currMax, num);
+                currMin = min(currMin, num);
+            }
+        }
+
+        return prevMax <= currMin;
+    }
+};
