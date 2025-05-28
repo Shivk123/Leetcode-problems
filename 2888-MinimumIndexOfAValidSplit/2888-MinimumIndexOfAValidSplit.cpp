8ad67dac7f5401e37ae0fc348d7fab5ec07b81ec
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:49:48 +0530

    Create 2888-MinimumIndexOfAValidSplit

---
+++ 2888-MinimumIndexOfAValidSplit/2888-MinimumIndexOfAValidSplit.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/28/2025, 9:49:47 PM
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        const int n = nums.size();
+        unordered_map<int, int> count1;
+        unordered_map<int, int> count2;
+
+        for (const int num : nums)
+            ++count2[num];
+
+        for (int i = 0; i < n; ++i) {
+            const int freq1 = ++count1[nums[i]];
+            const int freq2 = --count2[nums[i]];
+            if (freq1 * 2 > i + 1 && freq2 * 2 > n - 1 - i)
+                return i;
+        }
+
+        return -1;
+    }
+};
