60971cbb03220b4502f7c39625b05f77d38e95b7
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:56:07 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/28/2025, 9:56:06 PM
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+
+        while (--n > 0) {
+            string next;
+            for (int i = 0; i < ans.length(); ++i) {
+                int count = 1;
+                while (i + 1 < ans.length() && ans[i] == ans[i + 1]) {
+                    ++count;
+                    ++i;
+                }
+                next += to_string(count) + ans[i];
+            }
+            ans = std::move(next);
+        }
+
+        return ans;
+    }
+};
