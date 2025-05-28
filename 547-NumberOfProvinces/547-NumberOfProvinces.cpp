08add8e0015e6ec87018eab046d92784254fe8b8
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:54:44 +0530

    Create 547-NumberOfProvinces

---
+++ 547-NumberOfProvinces/547-NumberOfProvinces.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/28/2025, 9:54:43 PM
+class Solution {
+    private:
+    void dfs(int i,vector<vector<int>>& isConnected,vector<bool>& visited){
+        visited[i]=true;
+        for(int j=0;j<isConnected[i].size();j++){
+            if(isConnected[i][j]==1 && !visited[j])dfs(j,isConnected,visited);
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<bool> visited(isConnected.size());
+        int ans=0;
+        for(int i=0;i<isConnected.size();i++){
+            if(!visited[i]){
+                ans++;
+                dfs(i,isConnected,visited);
+            }
+        }
+        return ans;
+    }
+};
