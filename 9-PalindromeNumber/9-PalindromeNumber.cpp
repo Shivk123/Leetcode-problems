041d83032a880f959cd289727d1beb91ac2652c6
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:56:23 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/28/2025, 9:56:23 PM
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return false;
+        
+        string s = to_string(x);
+        int n=s.length();
+        for (int i =0; i< n/2; i++){
+            if(s[i]!=s[n-1-i])return false;
+        }
+        return true;
+    }
+};
