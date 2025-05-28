89b4975ff30ad802dd478492c53dad02531d046c
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:53:33 +0530

    Create 1335-MaximumCandiesAllocatedToKChildren

---
+++ 1335-MaximumCandiesAllocatedToKChildren/1335-MaximumCandiesAllocatedToKChildren.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/28/2025, 9:53:33 PM
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int l = 1;
+        int r = accumulate(candies.begin(), candies.end(), 0L) / k;
+
+        while (l < r) {
+            const int m = (l + r) / 2;
+            if (numChildren(candies, m) < k)
+                r = m;
+            else
+                l = m + 1;
+        }
+
+        return numChildren(candies, l) >= k ? l : l - 1;
+    }
+
+private:
+    long numChildren(const vector<int>& candies, int m) {
+        return accumulate(
+            candies.begin(), candies.end(), 0L,
+            [&](long subtotal, int c) { return subtotal + c / m; });
+    };
+};
