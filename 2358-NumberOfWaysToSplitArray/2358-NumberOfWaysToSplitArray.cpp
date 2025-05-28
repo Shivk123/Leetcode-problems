09697f6644e7b54e159d6dc40e99d2206616d5f0
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:51:12 +0530

    Create 2358-NumberOfWaysToSplitArray

---
+++ 2358-NumberOfWaysToSplitArray/2358-NumberOfWaysToSplitArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/28/2025, 9:51:11 PM
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int ans = 0;
+        long prefix = 0;
+        long suffix = accumulate(nums.begin(), nums.end(), 0L);
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            prefix += nums[i];
+            suffix -= nums[i];
+            if (prefix >= suffix)
+                ++ans;
+        }
+
+        return ans;
+    }
+};
