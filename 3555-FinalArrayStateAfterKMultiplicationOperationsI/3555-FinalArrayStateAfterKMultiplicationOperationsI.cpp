b657443539faf4f0ca0cbdb70a6ab4cdb2202114
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:48:32 +0530

    Create 3555-FinalArrayStateAfterKMultiplicationOperationsI

---
+++ 3555-FinalArrayStateAfterKMultiplicationOperationsI/3555-FinalArrayStateAfterKMultiplicationOperationsI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/28/2025, 9:48:31 PM
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int m) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+        }
+        while (k--) {
+            auto it = pq.top();
+            pq.pop();
+            nums[it.second] = m * it.first;
+            pq.push({m * it.first, it.second});
+        }
+        return nums;
+    }
+};
