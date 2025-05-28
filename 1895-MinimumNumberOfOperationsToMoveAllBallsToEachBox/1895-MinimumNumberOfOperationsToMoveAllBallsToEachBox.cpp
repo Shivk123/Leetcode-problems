625625c047c64e8ed9cd1ee696a43c8923afc03c
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:52:26 +0530

    Create 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox

---
+++ 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox/1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/28/2025, 9:52:25 PM
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> ans(boxes.length());
+
+        for (int i = 0, count = 0, moves = 0; i < boxes.length(); ++i) {
+            ans[i] += moves;
+            count += boxes[i] - '0';
+            moves += count;
+        }
+
+        for (int i = boxes.length() - 1, count = 0, moves = 0; i >= 0; --i) {
+            ans[i] += moves;
+            count += boxes[i] - '0';
+            moves += count;
+        }
+
+        return ans;
+    }
+};
