fcbbe320a7cce18bb561025914208a4d9d32bcbb
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:53:55 +0530

    Create 1049-MinimumDominoRotationsForEqualRow

---
+++ 1049-MinimumDominoRotationsForEqualRow/1049-MinimumDominoRotationsForEqualRow.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/28/2025, 9:53:54 PM
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int size = tops.size();
+
+        auto countMinRotations = [&](int x) {
+            int topCount = 0, bottomCount = 0;
+            for (int i = 0; i < size; ++i) {
+                if (tops[i] != x && bottoms[i] != x) {
+                    return size + 1;
+                }
+                topCount += tops[i] == x;
+                bottomCount += bottoms[i] == x;
+            }
+            return size - max(topCount, bottomCount);
+        };
+        int minRotations = min(countMinRotations(tops[0]), countMinRotations(bottoms[0]));
+
+        return minRotations > size ? -1 : minRotations;
+    }
+};
