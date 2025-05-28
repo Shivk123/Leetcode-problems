7c02628d892d6e4500dc93bea2e426a49a2c8bbb
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:53:27 +0530

    Create 1396-CountServersThatCommunicate

---
+++ 1396-CountServersThatCommunicate/1396-CountServersThatCommunicate.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/28/2025, 9:53:26 PM
+class Solution {
+ public:
+  int countServers(vector<vector<int>>& grid) {
+    const int m = grid.size();
+    const int n = grid[0].size();
+    int ans = 0;
+    vector<int> rows(m);
+    vector<int> cols(n);
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        if (grid[i][j] == 1) {
+          ++rows[i];
+          ++cols[j];
+        }
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        if (grid[i][j] == 1 && (rows[i] > 1 || cols[j] > 1))
+          ++ans;
+
+    return ans;
+  }
+};
