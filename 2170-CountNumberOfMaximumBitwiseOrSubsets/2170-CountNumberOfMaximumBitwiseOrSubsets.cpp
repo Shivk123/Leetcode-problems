1aa4704629c49824b91d726e6e0b72e99f0befbd
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:51:48 +0530

    Create 2170-CountNumberOfMaximumBitwiseOrSubsets

---
+++ 2170-CountNumberOfMaximumBitwiseOrSubsets/2170-CountNumberOfMaximumBitwiseOrSubsets.cpp
@@ -0,0 +1,34 @@
+// Last updated: 5/28/2025, 9:51:47 PM
+class Solution {
+public:
+    int maxOrValue;        
+    int subsetCount;
+    vector<int> numbers;
+    
+    int countMaxOrSubsets(vector<int>& nums) {
+        numbers = nums;     
+        maxOrValue = 0;     
+        subsetCount = 0;
+
+        for (int x : nums) {
+            maxOrValue |= x;
+        }
+
+        dfs(0, 0);
+      
+        return subsetCount;
+    }
+
+    void dfs(int index, int currentOrValue) {
+        if (index == numbers.size()) {
+            if (currentOrValue == maxOrValue) {
+                subsetCount++;
+            }
+            return;
+        }
+
+        dfs(index + 1, currentOrValue);
+
+        dfs(index + 1, currentOrValue | numbers[index]);
+    }
+};
