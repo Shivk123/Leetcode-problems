7ec675f2ed98ceea065c558517d259894063ea97
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:53:13 +0530

    Create 1500-CountLargestGroup

---
+++ 1500-CountLargestGroup/1500-CountLargestGroup.cpp
@@ -0,0 +1,20 @@
+// Last updated: 5/28/2025, 9:53:12 PM
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        vector<int> count(9 * 4 + 1);
+        for (int i = 1; i <= n; ++i)
+            ++count[getDigitSum(i)];
+        return ranges::count(count, ranges::max(count));
+    }
+
+private:
+    int getDigitSum(int num) {
+        int digitSum = 0;
+        while (num > 0) {
+            digitSum += num % 10;
+            num /= 10;
+        }
+        return digitSum;
+    }
+};
