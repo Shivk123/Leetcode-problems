e4f5424a0aea1fdbe6adda87874fb66242a0f3a4
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:48:46 +0530

    Create 3430-CountDaysWithoutMeetings

---
+++ 3430-CountDaysWithoutMeetings/3430-CountDaysWithoutMeetings.cpp
@@ -0,0 +1,20 @@
+// Last updated: 5/28/2025, 9:48:45 PM
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        int freeDays = 0;
+        int prevEnd = 0;
+
+        ranges::sort(meetings);
+
+        for (const vector<int>& meeting : meetings) {
+            const int start = meeting[0];
+            const int end = meeting[1];
+            if (start > prevEnd)
+                freeDays += start - prevEnd - 1;
+            prevEnd = max(prevEnd, end);
+        }
+
+        return freeDays + max(0, days - prevEnd);
+    }
+};
