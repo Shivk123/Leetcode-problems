b9d37dd6ab655f0aada4f7bdf697aaa71a000091
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:48:37 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/28/2025, 9:48:36 PM
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        const int n = colors.size();
+        int ans = 0;
+        int alternating = 1;
+
+        for (int i = 0; i < n + k - 2; ++i) {
+            alternating =
+                colors[i % n] == colors[(i - 1 + n) % n] ? 1 : alternating + 1;
+            if (alternating >= k)
+                ++ans;
+        }
+
+        return ans;
+    }
+};
