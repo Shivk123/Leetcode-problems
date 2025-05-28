fa09b77e673e2d019b228a5615970c23ca31d16e
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-05-28 21:50:24 +0530

    Create 2626-CountTheNumberOfGoodSubarrays

---
+++ 2626-CountTheNumberOfGoodSubarrays/2626-CountTheNumberOfGoodSubarrays.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/28/2025, 9:50:23 PM
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        long ans = 0;
+        int pairs = 0;
+        unordered_map<int, int> count;
+
+        for (int l = 0, r = 0; r < nums.size(); ++r) {
+            pairs += count[nums[r]]++;
+            while (pairs >= k)
+                pairs -= --count[nums[l++]];
+            ans += l;
+        }
+
+        return ans;
+    }
+};
