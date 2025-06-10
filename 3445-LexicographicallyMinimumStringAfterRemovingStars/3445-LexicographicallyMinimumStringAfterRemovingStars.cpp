fdbb7f8952df67f9e4d0df88b7d2753a56649c8f
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-06-10 18:34:32 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/10/2025, 6:34:31 PM
+class Solution {
+public:
+    string clearStars(string s) {
+        string ans = s;
+        vector<vector<int>> buckets(26);
+
+        for (int i = 0; i < s.length(); ++i)
+            if (s[i] == '*') {
+                ans[i] = ' ';
+                int j = 0;
+                while (buckets[j].empty())
+                    ++j;
+                ans[buckets[j].back()] = ' ', buckets[j].pop_back();
+            } else {
+                buckets[s[i] - 'a'].push_back(i);
+            }
+
+        std::erase(ans, ' ');
+        return ans;
+    }
+};
