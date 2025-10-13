69d01113fdd72fd1da9bbce74b03db70a555e6b0
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-10-13 19:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (96.49%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,9 +4,10 @@ public:
         int j=0;
         for(int i=1;i<nums.size();i++){
             if(nums[j]!=nums[i]){
-                nums[++j]=nums[i];
+                j++;
+                nums[j]=nums[i];
             }
         }
-        return j+1;
+        return ++j;
     }
 };
