a19d3b608ec8871f9c16c5e42c5cff3972a2b3e2
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-10-14 22:23:00 +0530

    Time: 411 ms (5.03%) | Memory: 20.7 MB (58.02%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i: nums){
-            ans^=i;
+        int n= nums.size();
+        //ek for loop
+        for(int i=0;i<n;i++){
+            //2 for loop
+            bool no_duplicate=true;
+            for(int j=0;j<n;j++){
+                if(i!=j && nums[i]==nums[j]){
+                    no_duplicate=false;
+                    break;
+                }
+            }
+            if(no_duplicate)return nums[i];
         }
-        return ans;
+        return -1;
     }
 };
