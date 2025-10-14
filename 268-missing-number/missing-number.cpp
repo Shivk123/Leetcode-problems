555b8bb0eb8ae5afc5b314b2290ce86a49934e71
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-10-14 21:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.02%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int ans=0;
+        int ans=n;
         //nums ka XOR
         for(int i=0;i<n;i++){
-            ans^=nums[i];
+            ans^=i^nums[i];
         }        
-        //Actual num ka XOR
-        for(int i=0;i<=n;i++){
-            ans^=i;
-        }
         return ans;
     }
 };
