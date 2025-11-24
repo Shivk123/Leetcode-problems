e319c92b5344eb7aa58f6efc035b27180722793e
Shivam Khetan <103183072+Shivk123@users.noreply.github.com>
2025-11-24 16:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (8.21%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -4,8 +4,9 @@ public:
         int pre=0;
         vector<bool> ans;
         for(int it: nums){
-            pre=(pre*2)%5+it;
-            ans.push_back((pre%5==0));
+            pre=(pre*2)+it;
+            pre=pre%5;
+            ans.push_back((pre==0));
         }
         return ans;
     }
